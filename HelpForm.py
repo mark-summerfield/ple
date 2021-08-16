13255c92692b21b7866832e11458fa12023f68d7
mark-summerfield <mark@qtrac.eu>
2021-08-16 09:31:30 +0100

    more

--- HelpForm.py
+++ HelpForm.py
@@ -4,9 +4,11 @@
 
 import tkinter as tk
 import tkinter.font as tkfont
+import tkinter.scrolledtext as tkscrolledtext
 import tkinter.ttk as ttk
 
-from Const import APPNAME
+import Config
+from Const import APPNAME, NSWE, PAD, WE
 
 
 class Form(tk.Toplevel):
@@ -16,14 +18,30 @@ class Form(tk.Toplevel):
         self.title(f'Help — {APPNAME}')
         self.geometry('640x480')
         # TODO
-        self.make_bindings()
+        config = Config.config
+        size = config.base_font_size
+        default_font = tkfont.nametofont('TkDefaultFont')
+        title_font = tkfont.Font(family=default_font.cget('family'),
+                                 size=size + 2, underline=True)
+        text = tkscrolledtext.ScrolledText(self)
+        text.tag_config('title', font=title_font, foreground='navy',
+                        justify=tk.CENTER)
+        table_title_font = default_font.copy()
+        table_title_font.configure(underline=True)
+        text.tag_config('row', tabs=('4c',), font=table_title_font,
+                        foreground='darkgreen')
+        text.insert(tk.END, 'Help\n', ('title',))
+        text.insert(tk.END, 'Shortcut\tAction\n', ('row',))
+        text.config(state=tk.DISABLED)
+        text.grid(row=0, column=0, sticky=NSWE)
+        ttk.Button(self, text='OK', underline=0, command=self.quit).grid(
+                   row=1, column=0)
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+        self.bind('<Return>', self.quit)
+        self.bind('<Alt-o>', self.quit)
+        self.bind('<Escape>', self.quit)
 
 
     def quit(self, _event=None):
         self.destroy()
-
-
-    def make_bindings(self):
-        self.bind('<Return>', self.quit)
-        self.bind('<Alt-o>', self.quit)
-        self.bind('<Escape>', self.quit)
